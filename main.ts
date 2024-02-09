18efdf34bed529104c633fb78b03e9a453567780
SrAquaGenius <dimoisivic@gmail.com>
2024-02-09 09:33:34 +0000

    Automaticly converts callouts to navbar elements but a lot of work is needed

--- main.ts
+++ main.ts
@@ -19,8 +19,9 @@ export default class CustomNavbar extends Plugin
 		await this.loadSettings();
 		await this.saveSettings();
 
-		this.registerEvent(this.app.workspace.on("file-open",
-			() => { this.searchInOpenFile(); }));
+		this.registerEvent(this.app.workspace.on("file-open", () => {
+			this.searchInOpenFile();
+		}));
 	}
 
 	onunload() {}
@@ -37,97 +38,89 @@ export default class CustomNavbar extends Plugin
 
 	async searchInOpenFile()
 	{
-		// const { vault } = this.app;
-		// const files: TFile[] = vault.getMarkdownFiles();
-		// const navbar_files: TFile[] = [];
-		// const str = "!navbar";
-
-		// for (const file of files) {
-		// 	const content = await vault.cachedRead(file);
-	
-		// 	if (content.includes(str)) {
-		// 		navbar_files.push(file); // Add the file to navbar_files array
-		// 		// console.log(content);
-		// 	}
-		// }
-			
-		// console.log("Files:", navbar_files.length);
-		// navbar_files.forEach(nav => {console.log(nav.path);});
-
-
 		const { workspace } = this.app;
 
 		const leaf_html = (workspace.getLeaf()).containerEl;
-		// console.log(leaf_html);
-
 		const callouts = leaf_html.querySelectorAll('.callout[data-callout="navbar"]');
-		if (callouts === null) {
-			console.log("No callouts found!");
-		}
-		else {
-			console.log("Found navbar callouts|");
-			
-		}
 
-		// callouts.forEach((callout: any) => { this.convertToNavBar(callout); });
+		if (callouts.length === 0)
+			console.log("No callouts found!", callouts);
+
+		else
+		{
+			console.log("Found navbar callouts!", callouts);
+			for (let c = 0; c < callouts.length; c++)
+				this.convertToNavBar(callouts[c]);
+		}
 	}
 
-	convertToNavBar(nav: any)
+	convertToNavBar(navbar: any)
 	{
-		console.log(nav);
+		console.log(navbar);
 
 		// Customize the new navbar
-		nav.classList.add('navbar');
-		nav.classList.remove('callout');
-		const nav_data = nav.getAttribute('data-callout-metadata');
-		if (nav_data !== null) nav.setAttribute('navbar-data', nav_data);
-		nav.removeAttribute('data-callout-metadata');
-		nav.removeAttribute('data-callout-fold');
-		nav.removeAttribute('data-callout');
+		navbar.classList.add('navbar');
+		navbar.classList.remove('callout');
+		const navbar_data = navbar.getAttribute('data-callout-metadata');
+		if (navbar_data !== null) navbar.setAttribute('navbar-data', navbar_data);
+		navbar.removeAttribute('data-callout-metadata');
+		navbar.removeAttribute('data-callout-fold');
+		navbar.removeAttribute('data-callout');
 
 		// Customize the new navbar header
-		const nav_header = this.ChangeClass(
-			nav, 'callout-title', 'navbar-header'
-		);
-		this.ChangeClass(nav_header, 'callout-icon', 'navbar-icon');
-		this.ChangeClass(nav_header, 'callout-title-inner', 'navbar-title');
-
-		let nav_sections = nav_header.querySelector('.navbar-sections');
-		if (nav_sections === null)
-		{
-			nav_sections = document.createElement('div');
-			nav_sections.classList.add('navbar-sections');
-			nav_header.appendChild(nav_sections);
-		}
+		const navbar_sections = this.convertToNavHeader(navbar);
 
 		// Customize the new navbar content
-		const nav_content = this.ChangeClass(
-			nav, 'callout-content', 'navbar-content'
+		this.convertToNavContent(navbar, navbar_sections);
+
+		console.log("ConvertToNavBar()->#end", navbar);
+	}
+
+	convertToNavHeader(navbar: any)
+	{
+		const navbar_header = this.changeClass(
+			navbar, 'callout-title', 'navbar-header'
+		);
+		this.changeClass(navbar_header, 'callout-icon', 'navbar-icon');
+		this.changeClass(navbar_header, 'callout-title-inner', 'navbar-title');
+
+		let navbar_sections = navbar_header.querySelector('.navbar-sections');
+		if (navbar_sections === null)
+		{
+			navbar_sections = document.createElement('div');
+			navbar_sections.classList.add('navbar-sections');
+			navbar_header.appendChild(navbar_sections);
+		}
+
+		return navbar_sections;
+	}
+
+	convertToNavContent(navbar: any, navbar_sections: any)
+	{
+		const navbar_content = this.changeClass(
+			navbar, 'callout-content', 'navbar-content'
 		);
 
 		// Customize each new navbar section
-		const sections = nav_content.querySelectorAll('.callout.is-collapsible.is-collapsed, .callout.is-collapsible, .callout');
-		let id = 1;
-		for (let section of sections)
+		const sections = navbar_content.querySelectorAll('.callout.is-collapsible.is-collapsed, .callout.is-collapsible, .callout');
+
+		for (let s = 0; s < sections.length; s++)
 		{
-			this.ConvertToNavSection(section, id);
+			let section = sections[s];
+			this.convertToNavSection(section, (s + 1));
 
 			// Re-organize the header and the contents
 			let header = section.querySelector('.navbar-section-header');
 			let content = section.querySelector('.navbar-section-content');
 
-			nav_sections.appendChild(header);
-			nav_content.appendChild(content);
+			navbar_sections.appendChild(header);
+			navbar_content.appendChild(content);
 
-			nav_content.removeChild(section);
-
-			id++;
+			navbar_content.removeChild(section);
 		}
-
-		console.log("ConvertToNavBar()->#end", nav);
 	}
 
-	ConvertToNavSection(section: any, id: any)
+	convertToNavSection(section: any, id: number)
 	{
 		console.log("ConvertToNavSection():", section, id);
 
@@ -148,24 +141,24 @@ export default class CustomNavbar extends Plugin
 		section.removeAttribute('data-callout');
 
 		// Customize the new navbar section header
-		const section_header = this.ChangeClass(
+		const section_header = this.changeClass(
 			section, 'callout-title', 'navbar-section-header'
 		);
 		section_header.setAttribute('navbar-id', id);
 		section_header.setAttribute('navbar-section-data', section_data);
 		section_header.setAttribute('is-collapsed', fold);
 
-		this.ChangeClass(section_header, 'callout-icon', 'navbar-section-icon');
-		this.ChangeClass(section_header, 'callout-title-inner', 'navbar-section-title');
+		this.changeClass(section_header, 'callout-icon', 'navbar-section-icon');
+		this.changeClass(section_header, 'callout-title-inner', 'navbar-section-title');
 
-		const section_fold = this.ChangeClass(
+		const section_fold = this.changeClass(
 			section_header, 'callout-fold', 'navbar-section-fold'
 		);
 		section_fold.classList.remove('is-collapsed');
 		section_fold.setAttribute('is-collapsed', fold);
 
 		// Customize the new navbar section content
-		const section_content = this.ChangeClass(
+		const section_content = this.changeClass(
 			section, 'callout-content', 'navbar-section-content'
 		);
 		section_content.setAttribute('navbar-id', id);
@@ -174,7 +167,7 @@ export default class CustomNavbar extends Plugin
 	}
 
 	/* --------------------------------------------------------------------- */
-	ChangeClass(parent: any, oldClass: any, newClass: any)
+	changeClass(parent: any, oldClass: any, newClass: any)
 	{
 		const element = parent.querySelector('.' + oldClass);
 		if (element !== null)
@@ -186,9 +179,35 @@ export default class CustomNavbar extends Plugin
 
 		else return parent.querySelector('.' + newClass);
 	}
+
+	/* --------------------------------------------------------------------- */
+	toggleSection(navbar, header, content)
+	{
+		console.log("Togfgling");		
+	}
 }
 
 /*
+
+// const { vault } = this.app;
+		// const files: TFile[] = vault.getMarkdownFiles();
+		// const navbar_files: TFile[] = [];
+		// const str = "!navbar";
+
+		// for (const file of files) {
+		// 	const content = await vault.cachedRead(file);
+	
+		// 	if (content.includes(str)) {
+		// 		navbar_files.push(file); // Add the file to navbar_files array
+		// 		// console.log(content);
+		// 	}
+		// }
+			
+		// console.log("Files:", navbar_files.length);
+		// navbar_files.forEach(nav => {console.log(nav.path);});
+
+
+
 // This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
